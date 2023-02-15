7d3ff4af3045420b1e5772d2254305ff8ea62b61
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2023-02-15 15:43:33 -0500

    demo-1

---
+++ kvs/KVS.cs
@@ -0,0 +1,74 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+using System.Web;
+using System.Text;
+
+namespace AMAUpdateSample
+{
+    public class KVS
+    {
+        private static Dictionary<string, string> _inMemoryKVS = new Dictionary<string, string>();
+
+        private readonly ILogger _logger;
+
+        public KVS(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<KVS>();
+        }
+
+        [Function("set")]
+        public async Task<HttpResponseData> Set([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("SET invoked");
+
+
+            var querystring = HttpUtility.ParseQueryString(req.Url.Query);
+            var key = querystring["key"];
+
+            if (String.IsNullOrWhiteSpace(key)) {
+                _logger.LogWarning("missing key in querystring");
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            // DEBUG! - REMOVE BEFORE PUSHING
+            if (key == "banana") {
+                _inMemoryKVS.Clear();
+                _logger.LogWarning("cleared in-memory KVS");
+                return req.CreateResponse(HttpStatusCode.Gone);
+            }
+
+            var value = await req.ReadAsStringAsync();
+
+            _inMemoryKVS[key] = value ?? string.Empty;
+
+            return req.CreateResponse(HttpStatusCode.NoContent);
+        }
+
+        [Function("get")]
+        public HttpResponseData Get([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+        {
+            _logger.LogInformation("GET invoked");
+
+            var querystring = HttpUtility.ParseQueryString(req.Url.Query);
+            var key = querystring["key"];
+
+            if (String.IsNullOrWhiteSpace(key)) {
+                _logger.LogWarning("missing key in querystring");
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            var value = string.Empty;
+            if (_inMemoryKVS.ContainsKey(key)) {
+                value = _inMemoryKVS[key];
+                var response = req.CreateResponse(HttpStatusCode.OK);
+                response.Body = new MemoryStream(Encoding.UTF8.GetBytes(value));
+                return response;
+            } else {
+                return req.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
